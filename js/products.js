45bbdb91770496134dc182b152ff313c1b7960f6
percas93 <110793667+percas93@users.noreply.github.com>
2022-08-10 16:15:44 -0300

    16:15 10/8

--- js/products.js
+++ js/products.js
@@ -1,14 +1,15 @@
-let productos = [];
+let autosArray = []; /*Variable donde guardar el return del getJSONData
+                utilizamos una array para poder recorrerlo con un for*/
 
 document.addEventListener("DOMContentLoaded", function(){
-        getJSONData(CATEGORIES_URL)
-        .then(function(){
-            if 
-        })
-})
+        getJSONData(PRODUCTS_URL+"101.json")
+        .then(function(result){ 
+            if (result.status === 'ok') {
+                autosArray  = result.data.products; //Guarda el return del getJSONDATA en variable autos
+            }
 
-
-
+       });
+});
 
 /*Referencia Ejercicio 4.6 
 
@@ -22,3 +23,28 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 });
 */
+
+function showProductsList(){
+
+    for(let i = 0; i < autosArray.length; i++){
+        agregarAlHTML = `
+            <div onclick="setCatID(${autosArray[i].id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${autosArray[i].image}" alt="${autosArray[i].description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${autosArray[i].name}</h4>
+                            <small class="text-muted">${autosArray[i].productCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${autosArray[i].description}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        document.getElementById("prod-list-container").innerHTML += agregarAlHTML;
+        }
+}
+
+showProductsList();
