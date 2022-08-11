5f46118ac0f5ebb91afd6d0bbea0451451cf4c01
percas93 <110793667+percas93@users.noreply.github.com>
2022-08-11 15:47:10 -0300

    Todo bajo el paraguas de addEvent

--- js/products.js
+++ js/products.js
@@ -1,15 +1,30 @@
 let autosArray = []; /*Variable donde guardar el return del getJSONData
                 utilizamos una array para poder recorrerlo con un for*/
 
-document.addEventListener("DOMContentLoaded", function () {
-    getJSONData(PRODUCTS_URL + "101.json")
-        .then(function (result) {
-            if (result.status === 'ok') {
-                autosArray = result.data.products; //Guarda el return del getJSONDATA en variable autos
-            }
+function showProductsList() {
 
-        });
-});
+    let agregarAlHTML = ""
+    for (let i = 0; i < autosArray.length; i++) {
+        agregarAlHTML += `
+            <div onclick="setCatID(${autosArray[i].id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${autosArray[i].image}" alt="${autosArray[i].description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${autosArray[i].name}</h4>
+                            <small class="text-muted">${autosArray[i].productCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${autosArray[i].description}</p>
+                    </div>
+                </div>
+            </div>
+            `
+    }
+    document.getElementById("prod-list-container").innerHTML += agregarAlHTML;
+
+}
 
 /*Referencia Ejercicio 4.6 
 
@@ -24,29 +39,13 @@ document.addEventListener("DOMContentLoaded", function(e){
 });
 */
 
-function showProductsList() {
+document.addEventListener("DOMContentLoaded", function () {
+    getJSONData(PRODUCTS_URL + "101.json")
+        .then(function (result) {
+            if (result.status === 'ok') {
+                autosArray = result.data.products; //Guarda el return del getJSONDATA en variable autos
+            }
 
-    let agregarAlHTML = "<p>Párrafo</p>"
-    /*for (let i = 0; i < autosArray.length; i++) {
-        agregarAlHTML += `
-            <div onclick="setCatID(${autosArray[2].id})" class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="${autosArray[2].image}" alt="${autosArray[2].description}" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${autosArray[2].name}</h4>
-                            <small class="text-muted">${autosArray[2].productCount} artículos</small>
-                        </div>
-                        <p class="mb-1">${autosArray[2].description}</p>
-                    </div>
-                </div>
-            </div>
-            `
-    }*/
-    document.getElementById("prod-list-container").innerHTML += agregarAlHTML;
-
-}
-
-showProductsList();
+        });
+    showProductsList();
+});
