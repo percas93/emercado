e5a65bf2bcf17c4a1d8b22c7e2cd1e75e6497506
percas93 <110793667+percas93@users.noreply.github.com>
2022-08-16 11:34:09 -0300

    .value ?

--- js/login.js
+++ js/login.js
@@ -2,9 +2,8 @@ var btnEnviar = document.getElementById("btnEnviar");
 var mail = document.getElementById("mail");
 var pass = document.getElementById("pass"); 
 
-
 btnEnviar.addEventListener('click', () => {
-    if (mail != '' && pass != '') {
+    if (mail.value != '' && pass.value != '') {
         window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }
     else{
