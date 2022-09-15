e935c65a6a127c6956ff8303a0bcaf0bd59b627a
percas93 <110793667+percas93@users.noreply.github.com>
2022-09-15 20:10:05 -0300

    Probando la lógica

--- js/product-info.js
+++ js/product-info.js
@@ -14,29 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-/*
 //FUNCIÓN QUE COMPLETA EL HTML A PARTIR DEL OBJETO specificProduct
 function showProductsInfo() {
 
-    let textoaAgregar =
-        `
-            <div class="list-group-item list-group-item-action cursor-active" onclick="localStorage.setItem('prodID', ${productsArray[i].id}); window.location='product-info.html';">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="${productsArray[i].image}" alt="${productsArray[i].description}" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${productsArray[i].name} - ${productsArray[i].cost} ${productsArray[i].currency}</h4>
-                            <small class="text-muted">${productsArray[i].soldCount} vendidos</small>
-                        </div>
-                        <p class="mb-1">${productsArray[i].description}</p>
-                    </div>
-                </div>
-            </div>
-            `
+    document.getElementById("prod-name-to-show").textContent = specificProduct.name
 
-    
-document.getElementById("prod-info-container").innerHTML = textoaAgregar;
-
-}*/
+}
