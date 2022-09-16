26544417b4e262e78ba2df8b6896a0c095468e1f
percas93 <110793667+percas93@users.noreply.github.com>
2022-09-15 22:16:16 -0300

    Para todos los campos

--- js/product-info.js
+++ js/product-info.js
@@ -17,8 +17,8 @@ document.addEventListener("DOMContentLoaded", function () {
 //FUNCIÓN QUE COMPLETA EL HTML A PARTIR DEL OBJETO specificProduct
 function showProductInfo() {
 
-    document.getElementById("prod-name-to-show").textContent = specificProduct.currency + " " + specificProduct.name
-    document.getElementById("prod-cost-to-show").textContent = specificProduct.cost
+    document.getElementById("prod-name-to-show").textContent = specificProduct.name
+    document.getElementById("prod-cost-to-show").textContent = specificProduct.currency + " " + specificProduct.cost
     document.getElementById("prod-desc-to-show").textContent = specificProduct.description
     document.getElementById("prod-cat-to-show").textContent = specificProduct.category
     document.getElementById("prod-u-sold-to-show").textContent = specificProduct.soldCount
