d0de48a9696b3c3efcaa0219a75782affe2f43ed
percas93 <110793667+percas93@users.noreply.github.com>
2022-09-17 11:14:48 -0300

    probemos

--- js/product-info.js
+++ js/product-info.js
@@ -23,7 +23,7 @@ function showProductInfo() {
     document.getElementById("prod-u-sold-to-show").textContent = specificProduct.soldCount
     specificProduct.images.forEach(pic => {
         document.getElementById("img-grid").innerHTML += 
-        "<div id='col' width='auto' height='auto'><img src=" + pic + "><div>"
+        "<div class='col'><img width=auto height=auto src=" + pic + "><div>"
     });
 
 }
