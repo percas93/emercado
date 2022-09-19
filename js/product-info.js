cc54de86569535a245f0bd15187b6f68b22deec1
percas93 <110793667+percas93@users.noreply.github.com>
2022-09-19 12:36:01 -0300

    pronto esto?

--- js/product-info.js
+++ js/product-info.js
@@ -61,7 +61,7 @@ function showCommentSection() {
         <li class="list-group-item d-flex justify-content-between col">
               <div>
                 <div style="display: inline-flex">
-                    <h4 class="my-0">${commentsArray[j].user}</h4><p>      ${commentsArray[j].dateTime}     </p>
+                    <h4 class="my-0">${commentsArray[j].user}</h4><p class="text-muted">${commentsArray[j].dateTime}</p>
                     <span class="fa fa-star" id="com${contador}-star1"></span>
                     <span class="fa fa-star" id="com${contador}-star2"></span>
                     <span class="fa fa-star" id="com${contador}-star3"></span>
@@ -72,7 +72,7 @@ function showCommentSection() {
               </div>
             </li>
         `
-        for (let num = 1; num < commentsArray[j].score && num > 0; num++) {
+        for ( let num = 1; num <= commentsArray[j].score && num > 0; num++) {
             document.getElementById("com" + contador + "-star" + num).setAttribute('class', 'fa fa-star checked');
         }
         contador += 1
