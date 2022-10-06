e1f0886b8bc2862170e5fdfebbe831eed4f7a335
percas93 <110793667+percas93@users.noreply.github.com>
2022-10-06 12:26:06 -0300

    Ya debieran verse algunos resultados

---
+++ js/cart.js
@@ -0,0 +1,36 @@
+arrayCart = []
+
+//EJECUTAR FUNCIONES PARA VER CONTENIDO AL CARGAR
+document.addEventListener("DOMContentLoaded", function () {
+
+    getJSONData(CART_INFO_URL + 25801 + ".json")
+        .then(function (result) {
+            if (result.status === 'ok') {
+                auxArray = result.data;
+                gettingJSONCart(auxArray, arrayCart);
+                gettingLSCart();
+            }
+        })
+});
+
+//FUNCIÓN QUE MUEVE PRODUCTOS DEL CARRITO DE COMPRAS SEGÚN URL AL ARRAY OPERATIVO
+function gettingJSONCart(arrayi, arrayf) {
+    arrayi.forEach(product => {
+        object = { id: product.id, count: product.count }
+        arrayf.push(object);
+    });
+}
+
+//FUNCIÓN QUE MUEVE PRODUCTOS DEL CARRITO DE COMPRAS SEGÚN LOCALSTORAGE AL ARRAY OPERATIVO
+function gettingLSCart() {
+    let itemsCount = localStorage.getItem("itemsCount"); //itemsCount es un contador de guardados en LS
+    for (let i; i < itemsCount; i++) {
+        object = localStorage.getItem("cartProd" + i).json
+        if (arrayCart.some(e => e.id === object.id)) {//Si se repite un producto guardado..
+            arrayCart.find(e => e.id === object.id).count = object.count //..sólo cambiamos su cantidad
+        }
+        else { //Si el producto no se repite, lo agregamos al array auxiliar prodFromLS
+            arrayCart.push(object);
+        }
+    }
+};
