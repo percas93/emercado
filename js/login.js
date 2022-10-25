2a8fa52de84214619c6cc602ba1cc7e936ce3fb1
percas93 <110793667+percas93@users.noreply.github.com>
2022-10-25 13:28:42 -0300

    looking clean

--- js/login.js
+++ js/login.js
@@ -4,9 +4,9 @@ var pass = document.getElementById("pass");
 
 btnEnviar.addEventListener('click', () => {
     if (mail.value != '' && pass.value != '') {
-        window.location = "index.html";
         localStorage.setItem("loginStatus", "logged");
         localStorage.setItem("user", mail.value);
+        window.location = history.back();
     }
     else{
         alert("Existen campos sin completar");
