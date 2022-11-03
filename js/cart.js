ab75d0258bff903692b83eaedfdeb6d7c76abbc0
percas93 <110793667+percas93@users.noreply.github.com>
2022-11-03 00:48:06 -0300

    las ideas llegan

--- js/cart.js
+++ js/cart.js
@@ -3,39 +3,38 @@ arrayCart = []
 //EJECUTAR FUNCIONES PARA VER CONTENIDO AL CARGAR
 document.addEventListener("DOMContentLoaded", function () {
 
-    showUserButton();
+    showUserButton()
 
     loginCheck();
+
+    if (localStorage.getItem("localCart")) {//condicionado a la existencia de un carrito en localStorage
+        arrayCart = JSON.parse(localStorage.getItem("localCart"));
+    }//desde que puedo guardar cantidades modificando el carrito, cabe priorizar el localCart
 
     getJSONData(CART_INFO_URL + 25801 + ".json")
         .then(function (result) {
             if (result.status === 'ok') {
-                arrayCart = result.data.articles;
-                let auxLSCart = JSON.parse(localStorage.getItem("localCart"));
-                if (auxLSCart) {//condicionar la adición de objetos del localStorage a que efectivamente los haya
-                    concatNoDuplicates(arrayCart, auxLSCart);
-                }
+                let auxExtCart = result.data.articles;
+                concatNoDuplicates(arrayCart, auxExtCart);
                 showCartProds();
-                //updateRowSubtotals();
-                //updateSubtotals();
+                updateRowSubtotal();
+                updateSubtotal ()
             }
         })
 });
 
 //FUNCIÓN QUE AGREGA A UN ARRAY DESDE OTRO SIN REPETIR
-function concatNoDuplicates(finalArr, arrToAdd, pk = "id") {
+function concatNoDuplicates(finalArr, arrToAdd, pk = "id", overwrite = false) {
     arrToAdd.forEach(eArrToAdd => {
         if (!finalArr.some(eFinalArr => eArrToAdd[pk] == eFinalArr[pk])) {
             finalArr.push(eArrToAdd);
         }
-        //Este else se puede borrar si se quiere mantener los atributos de los objetos en el array inicial
-        else {
-            let eToRemoveIdx = finalArr.findIndex(object => {object[pk] == eArrToAdd[pk]});
-            finalArr.splice(eToRemoveIdx);
-            finalArr.push(eArrToAdd);
+        else if (overwrite) {//incluir esto si queremos que el objeto repetido sobreescriba al antiguo
+            let elementToReplace = finalArr.find(e => e[pk] == eArrToAdd[pk]);
+            Object.assign(elementToReplace, eArrToAdd);
         }
     });
-}//La redacción de esta función la pensé para su reutilización
+}//Esta función la pensé para su reutilización
 
 //FUNCIÓN QUE COMPLETA EL HTML A PARTIR DEL ARRAY COMPLETO DE OBJETOS DEL CARRITO
 function showCartProds() {
@@ -45,7 +44,7 @@ function showCartProds() {
             <td><img src="${product.image}" alt="Imagen del producto" class="img-fluid" style="height: 3vw"></td>
             <td>${product.name}</td>
             <td>${product.currency} ${product.unitCost}</td>
-            <td><input type="text" class="rowInput form-control" value="${product.count}" style="width:3em"></td>
+            <td><input type="number" class="rowInput form-control" value="${product.count}" style="width:4em"></td>
             <td>${product.currency} <span class="rowSubtotal">${product.unitCost * product.count}</span></td>
         </tr>
         `
@@ -54,30 +53,29 @@ function showCartProds() {
 };//no darle márgenes laterales a la tabla fue una elección estética
 
 //FUNCIÓN QUE OTORGA A LOS SUBTOTALES LA CAPACIDAD DE ACTUALIZARSE ANTE LA MODIFICACIÓN DE CANTIDADES
-function updateRowSubtotals() {
-    let everyInput = document.getElementsByClassName("rowInput");
-    let everySubtotal = document.getElementsByClassName("rowSubtotal");
+function updateRowSubtotal() {
+    let everyInput = document.getElementsByClassName("rowInput");//geT colección de nodos rowInputs
+    let everyRowSubtotal = document.getElementsByClassName("rowSubtotal");//get colección de nodos rowSubtotal
     for (let i = 0; i < everyInput.length; i++) {
         everyInput[i].oninput = function() {
-            updateArrayQuantities();//Actualizar cantidades en Array
-            everySubtotal[i].innerHTML = arrayCart[i].value * arrayCart[i].unitCost;//Actualizar subtotal del renglón
-            updateSubtotals();//Actualizar subtotales finales
+            everyRowSubtotal[i].innerHTML = everyInput[i].value * arrayCart[i].unitCost;//actualizar subtotal del renglón
+            arrayCart[i].count = JSON.parse(everyInput[i].value || 0);//actualizar cantidades en el array
+            updateLSCart();//actualizar el array del localStorage
+            updateSubtotal();//actualizar subtotal agregado
         };
     };
 };
 
-//FUNCIÓN QUE ACTUALIZA LAS CANTIDADES DEL ARRAY SEGÚN INPUTS
-function updateArrayQuantities() {
-    let everyInput = document.getElementsByClassName("rowInput");
-    for (let i = 0; i < arrayCart.length; i++) {
-        arrayCart[i].count = everyInput[i].value;
-    };
+//FUNCIÓN QUE GUARDA NUESTRO CARRITO ACTUALIZADO EN localStorage
+function updateLSCart () {
     localStorage.setItem("localCart", JSON.stringify(arrayCart));
 };
 
-//FUNCIÓN QUE ACTUALIZA LOS SUBTOTALES
-function updateSubtotals() {
+//FUNCIÓN QUE ACTUALIZA EL SUBTOTAL AGREGADO
+function updateSubtotal () {
     let subtotal = 0;
-    arrayCart.forEach(e => {subtotal += e.count * e.unitCost});
-    document.getElementById("productCostText").innerText = subtotal
-}
+    arrayCart.forEach(e => {
+        subtotal += e.unitCost * e.count;
+    });
+    document.getElementById("productCostText").textContent = subtotal;
+};
